8f91fe8f11a6a2ccf1020e5fccfe3652f22936b7
Takayuki Sei <takayuki_sei@voyagegroup.com>
2016-04-10 23:08:26 +0900

    傾きに合わせて見る方向を変えられるよう修正

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,32 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-// クラス名が被っているといけないので、namespaceで囲む
-namespace TAK_CameraController
+
+public class CameraController : MonoBehaviour
 {
-
-	public class CameraController : MonoBehaviour
+	void Update()
 	{
-		void Update()
-		{
-			Vector3 acceleration = Input.acceleration;
-
-			if (Input.GetKey(KeyCode.RightArrow)) {
-				Transform trans = this.transform;
-				trans.Rotate (Vector3.up, 1);
-			}
-			if (Input.GetKey(KeyCode.LeftArrow)) {
-				Transform trans = this.transform;
-				trans.Rotate (Vector3.up, -1);
-			}
-			if (Input.GetKey(KeyCode.UpArrow)) {
-				Transform trans = this.transform;
-				trans.Rotate (Vector3.left, 1);
-			}
-			if (Input.GetKey(KeyCode.DownArrow)) {
-				Transform trans = this.transform;
-				trans.Rotate (Vector3.left, -1);
-			}
-		}
+		Quaternion q = Input.gyro.attitude;
+		Quaternion qq = Quaternion.AngleAxis (-90.0f, Vector3.left);
+		q.x *= -1.0f;
+		q.y *= -1.0f;
+		this.transform.localRotation = qq * q;
 	}
+
 }
